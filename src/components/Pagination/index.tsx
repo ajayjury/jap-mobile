8436b510940d037dfb7c6312fc8f3c939a090ac3
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-07-10 20:59:16 +0530

    done

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -4,15 +4,17 @@ import { chevronBackOutline, chevronForwardOutline } from "ionicons/icons";
 type Props = {
     prev: () => void;
     next: () => void;
+    prev_disabled?: boolean;
+    next_disabled?: boolean;
 }
-const Pagination: React.FC<Props> = ({prev, next}) => {
+const Pagination: React.FC<Props> = ({prev, next, prev_disabled=true, next_disabled=true}) => {
     return (
         <IonRow className="ion-align-items-center ion-justify-center-center p-0 mt-1">
             <IonCol
                 size="6"
                 className='text-right'
             >
-                <IonButton className="pagination-btn m-0" fill='outline' color="success" onClick={prev}>
+                <IonButton className="pagination-btn m-0" fill='outline' color="success" disabled={prev_disabled} onClick={prev}>
                 <IonIcon slot="start" icon={chevronBackOutline}></IonIcon>
                     Prev
                 </IonButton>
@@ -21,7 +23,7 @@ const Pagination: React.FC<Props> = ({prev, next}) => {
                 size="6"
                 className='text-left'
             >
-                <IonButton className="pagination-btn m-0" fill='outline' color="success" onClick={next}>
+                <IonButton className="pagination-btn m-0" fill='outline' color="success" disabled={next_disabled} onClick={next}>
                     Next
                 <IonIcon slot="end" icon={chevronForwardOutline}></IonIcon>
                 </IonButton>
--- src/components/ProductSegment/index.tsx
+++ src/components/ProductSegment/index.tsx
@@ -1,4 +1,4 @@
-import { IonCol, IonGrid, IonLabel, IonRow, IonSegment, IonSegmentButton, } from "@ionic/react";
+import { IonCol, IonGrid, IonLabel, IonRow, IonSegment, IonSegmentButton, IonText, } from "@ionic/react";
 import { Link } from "react-router-dom";
 import ProductCard from "../ProductCard";
 import Pagination from "../Pagination";
@@ -32,25 +32,50 @@ const segments = [
 ];
 
 const ProductSegment: React.FC<Props> = ({category_slug}) => {
-
-    const [link, setLink] = useState<string>(category_slug ? api_routes.products+`?filter[has_categories]=${category_slug}` : api_routes.products)
     const [segment, setSegment] = useState<string>('default')
+    const [segmentUrl, setSegmentUrl] = useState<string>('')
     const [loading, setLoading] = useState<boolean>(false)
+    const [page, setPage] = useState<number>(1)
     const [products, setProducts] = useState<any[]>([]);
+    const [meta, setMeta] = useState<any>({
+        next: true,
+        prev: true,
+    });
 
     useEffect(() => {
       fetchProducts();
       return () => {}
-    }, [link])
+    }, [page, segmentUrl])
 
     const fetchProducts = async ():Promise<void> => {
         setLoading(true);
         try {
-            const response:AxiosResponse = await axiosPublic.get(link);
-            console.log(response.data)
+            let category_slug_link = api_routes.products+`?page=${page}`;
+            if(category_slug){
+                category_slug_link+=`?page=${page}&filter[has_categories]=${category_slug}`
+            }
+            const response:AxiosResponse = await axiosPublic.get(category_slug_link+segmentUrl);
             setProducts([...response.data.data])
+            if(response.data.links.next){
+                setMeta({
+                    ...meta, next: false
+                })
+            }else{
+                setMeta({
+                    ...meta, next: true
+                })
+            }
+            if(response.data.links.prev){
+                setMeta({
+                    ...meta, prev: false
+                })
+            }else{
+                setMeta({
+                    ...meta, prev: true
+                })
+            }
         } catch (error) {
-            
+            console.log(error);
         }finally{
             setLoading(false);
         }
@@ -59,11 +84,21 @@ const ProductSegment: React.FC<Props> = ({category_slug}) => {
     const segmentChangeHandler = (data:any):void => {
         setSegment(data.detail.value)
         if(data.detail.value==='default'){
-            setLink(api_routes.products+`?filter[has_categories]=${category_slug}`)
+            setSegmentUrl('')
             return;
         }
-        setLink(api_routes.products+`?filter[has_categories]=${category_slug}`+`&filter[${data.detail.value}]=true`)
+        setSegmentUrl(`&filter[${data.detail.value}]=true`)
     }
+
+    const prevHandler = ():void => {
+        setPage(page-1)
+    }
+    
+    const nextHandler = ():void => {
+        setPage(page+1)
+    }
+
+    console.log('fired')
     
     return (
         <>
@@ -72,35 +107,39 @@ const ProductSegment: React.FC<Props> = ({category_slug}) => {
             </div>
 
             <IonSegment scrollable={true} value={segment} color="success" onIonChange={(data)=>segmentChangeHandler(data)}>
-            {
-                segments.map((item, i)=><IonSegmentButton value={item.value} key={i}>
-                <IonLabel>{item.name}</IonLabel>
-                </IonSegmentButton>)
-            }
-            </IonSegment>
-            
-            {loading && <LoadingCard />}
-            
-            {!loading && <IonGrid className="mt-1 p-0">
-                <IonRow className="ion-align-items-center ion-justify-content-between p-0">
                 {
-                    products.map((item, i) => <IonCol
-                    size="6"
-                    size-xl="3"
-                    size-lg="3"
-                    size-md="4"
-                    size-sm="6"
-                    size-xs="6" className='p-0' key={i}
-                >
-                    <Link className="no-underline" to={`/products/${item.slug}`}>
-                        <ProductCard image={item.featured_image_link} name={item.name} price={`Rs. ${item.price}`} discounted_price={`Rs. ${item.discounted_price}`} />
-                    </Link>
-                </IonCol>)
+                    segments.map((item, i)=><IonSegmentButton value={item.value} key={i}>
+                    <IonLabel>{item.name}</IonLabel>
+                    </IonSegmentButton>)
                 }
+            </IonSegment>
 
-                </IonRow>
-                <Pagination prev={()=>alert('prev')} next={()=>alert('next')} />
-            </IonGrid>}
+            {/* {loading && <LoadingCard />} */}
+            
+            {loading ? <LoadingCard /> : products.length>0 ? <>
+                <IonGrid className="mt-1 p-0">
+                    <IonRow className="ion-align-items-center ion-justify-content-between p-0">
+                    {
+                        products.map((item, i) => <IonCol
+                        size="6"
+                        size-xl="3"
+                        size-lg="3"
+                        size-md="4"
+                        size-sm="6"
+                        size-xs="6" className='p-0' key={i}
+                    >
+                        <Link className="no-underline" to={`/products/${item.slug}`}>
+                            <ProductCard image={item.featured_image_link} name={item.name} price={`Rs. ${item.price}`} discounted_price={`Rs. ${item.discounted_price}`} />
+                        </Link>
+                    </IonCol>)
+                    }
+
+                    </IonRow>
+                    <Pagination prev={()=>prevHandler()} prev_disabled={meta.prev} next={()=>nextHandler()} next_disabled={meta.next} />
+                </IonGrid>
+            </> : <IonText color={'success'}>
+               <p className="text-center mt-1">Oops! No products available.</p>
+            </IonText>}
 
         </>
     );
